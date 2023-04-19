ab4bed7c5f93df7fe3106e4663538a5e75fb5372
Hafizov Shahboz <maestro_13@list.ru>
2023-04-19 18:57:32 +0300

    adding event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basic';
+  inputValueStr = ''
+
+  image: string = 'https://wallbox.ru/resize/1024x1024/wallpapers/main/201151/koshki-f60c1e13bc32.jpg'
+
+  changeTitleAndImg(): void {
+    this.title = 'Changed title'
+    this.image = 'https://topstorybox.com/wp-content/uploads/2023/01/cat.jpg'
+  }
+
+  inputValue(event: Event): void {
+    this.inputValueStr = (event.target as HTMLInputElement).value;
+  }
 }
