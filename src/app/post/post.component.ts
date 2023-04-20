4dc2db8795f3c6c2ad85b561d05bd68ca2752054
Hafizov Shahboz <maestro_13@list.ru>
2023-04-20 18:22:34 +0300

    change detection strategy

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,75 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
+import {Post} from "../app.component";
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PostComponent implements OnInit, OnChanges, DoCheck, AfterViewInit, AfterContentInit, AfterContentChecked ,AfterViewChecked ,OnDestroy{
+
+  @Input() post: any;
+  @Output() deleteEvent: EventEmitter<Post> = new EventEmitter<Post>()
+
+  firstTitle = 'First title'
+
+  constructor() {
+    console.log('constructor')
+    setTimeout(()=> {
+      this.firstTitle = 'Changed first title'
+    }, 2000)
+  }
+
+  deletePost(post: Post): void {
+      this.deleteEvent.emit(post)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes)
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy')
+  }
+
+}
