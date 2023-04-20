cffd2a3af4aa09c1d843cb9b338adff261a9f0ab
Hafizov Shahboz <maestro_13@list.ru>
2023-04-20 18:14:15 +0300

    lifecycle components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,15 @@ export class AppComponent {
   title = 'angular-basic';
   posts: Post[] = [
     {id: 1, title: 'First card', text:'description'},
-    {id: 2, title: 'Second card', text:'description'},
-    {id: 3, title: 'Third card', text:'description'}
+    // {id: 2, title: 'Second card', text:'description'},
+    // {id: 3, title: 'Third card', text:'description'}
   ]
 
   deletePost(event: Post): void {
     this.posts = this.posts.filter((post) => post.id !== event.id)
+  }
+
+  changePost(): void {
+    this.posts[0].title = 'Changed card'
   }
 }
