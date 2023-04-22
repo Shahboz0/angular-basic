69f14768e7264cf13462431c36a6c7ac3cc7e140
KhafizovS <maestro_13@list.ru>
2023-04-22 18:31:53 +0300

    addind table from angular material with error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home/home.component";
 import {AboutComponent} from "./about/about/about.component";
+import {TableComponent} from "./table/table/table.component";
+import {ChartComponent} from "./chart/chart/chart.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'table', component: TableComponent},
+  {path: 'chart', component: ChartComponent},
   {path: 'posts', loadChildren: () => import('./posts/posts/posts.module').then((item) => item.PostsModule)},
   {path: '', pathMatch: "full", redirectTo: 'home'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import {AppRoutingModule} from "./app-routing.module";
 import { HomeComponent } from './home/home/home.component';
 import { AboutComponent } from './about/about/about.component';
 import {PostsModule} from "./posts/posts/posts.module";
+import { TableComponent } from './table/table/table.component';
+import { ChartComponent } from './chart/chart/chart.component';
+import {MatTableModule} from "@angular/material/table";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
+    TableComponent,
+    ChartComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    PostsModule
+    PostsModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
